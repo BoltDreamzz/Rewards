71cb979576fe1d3a236dbc1eb47ba92cb7ae5909
Bolt Dreamzz <boltdreamz@gmail.com>
2025-01-17 12:29:32 +0100

    Version 1

--- Rewards/settings.py
+++ Rewards/settings.py
@@ -158,5 +158,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'myluxuryhomes2@gmail.com'
-EMAIL_HOST_PASSWORD = 'config("EMAIL_HOST_PASSWORD")'
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 ADMIN_EMAIL = "boltdreamz@gmail.com", "retireesrewards@gmail.com"
